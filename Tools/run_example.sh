d981fd949e302dad0183624abd33fd985c2ad000
Ekaterina Sakharova <kates@ebi.ac.uk>
2020-10-16 09:03:46 +0100

    added test examples

---
+++ Tools/run_example.sh
@@ -0,0 +1,5 @@
+python3 give_pathways.py -i test-input/test \
+                        -g ../graphs/make_graph/graphs.pkl \
+                        -n ../pathways/all_pathways_names.txt \
+                        -c ../pathways/all_pathways_class.txt \
+                        -o test-out
