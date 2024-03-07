b2defd57d01f4cd8769993dcf19fea2e0aef857c
Ekaterina Sakharova <49755902+KateSakharova@users.noreply.github.com>
2024-03-07 14:57:42 +0000

    Update run_pathways.sh

--- docker/run_pathways.sh
+++ docker/run_pathways.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-while getopts i: option; do
-	case "${option}" in
-		i) INPUT=${OPTARG};;
-	esac
+export INPUT=false
+export LIST_KO=false
+
+while getopts i:l: option; do
+    case "${option}" in
+        i) INPUT=${OPTARG};;
+        l) LIST_KO=${OPTARG};;
+    esac
 done
 
 export OUTPUT_PARSING=parsing_table
@@ -12,6 +16,14 @@ export OUTDIR=pathways
 mkdir -p ${OUTDIR}
 export OUTPUT=${OUTDIR}/result
 
-echo "pathways" && \
-python3 /tools/give_pathways.py -i ${INPUT} -g /pathways_data/graphs.pkl -c /pathways_data/all_pathways_class.txt -n /pathways_data/all_pathways_names.txt -o ${OUTPUT} && \
-echo "finish"
+if [ $INPUT ]; then
+    echo "Processing input file"
+    python3 /tools/give_pathways.py -i ${INPUT} -g /pathways_data/graphs.pkl -c /pathways_data/all_pathways_class.txt -n /pathways_data/all_pathways_names.txt -o ${OUTPUT_PARSING}
+    echo "Finished processing"
+fi
+
+if [ $LIST_KO ]; then
+    echo "Processing KO list"
+    python3 /tools/give_pathways.py -l ${LIST_KO} -g /pathways_data/graphs.pkl -c /pathways_data/all_pathways_class.txt -n /pathways_data/all_pathways_names.txt -o ${OUTPUT_UNION}
+    echo "Finished processing"
+fi
